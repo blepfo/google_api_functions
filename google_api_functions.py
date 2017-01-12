aed512bf8c8ea1b09fce330f17fd8723222cb56e
blepfo <blepfo@gmail.com>
2017-01-12 17:33:33 +0000

    Made object-oriented to reduce code duplication and improve usability

--- google_api_functions.py
+++ google_api_functions.py
@@ -3,6 +3,8 @@
 import httplib2
 # For retrieving and storing credentials
 import os
+# For reading from an app_details.json
+import json
 
 # Google API modules
 from apiclient import discovery
@@ -19,110 +21,134 @@ flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 from email.mime.text import MIMEText
 import base64
 
+def get_api_object(path):
+	""" Gets details from an app_details.json and returns Google_API object
+	created using the information. We assume the JSON has:
+	{ 
+		"APPLICATION_NAME" : "App Name",
+		"CLIENT_SECRET_FILE" : "path/to/file",
+		"SCOPES" : "path/to/file",
+		"CREDENTIALS_PATH" : "path/to/file"
+	}
+	We also assume that the scopes file has each scope on its own line
+	"""
+	with open(path, 'r') as app_detailsJSON:
+		app_details = json.loads(app_detailsJSON.read())
+		api_object = Google_API(
+							APPLICATION_NAME=app_details["APPLICATION_NAME"],
+							CLIENT_SECRET_FILE=app_details["CLIENT_SECRET_FILE"],
+							SCOPES=read_scopes(app_details["SCOPES"]),
+							CREDENTIALS_FILE=app_details["CREDENTIALS_FILE"])
+	return api_object
 
-"""--- Information for Authorization ---"""
-APPLICATION_NAME = 'APPLICATION NAME'
-# Update this with the location of your client_secret.json, which
-# allows your application to talk to Google's servers
-CLIENT_SECRET_FILE = '/home/pi/google_credentials/client_secret.json'
-# Scopes define what permissions you application needs.
-# If you are storing scopes in a file, be sure to change the location
-with open('/home/pi/google_credentials/scopes.txt', 'r') as scopes_file:
-	# In scopes.txt, each scope is stored by itself on a single line
-	SCOPES = scopes_file.read().splitlines()
-	SCOPES = " ".join(SCOPES)
-
-
-def get_credentials():
-  """ Retreive user credentials so we can access the spreadsheet.
-  This function works as follows:
-  1) Check if there are credentials already stored (in Storage object)
-  2) If there are, return the Credentials object (using store.get())
-  3) If not, initiate flow_from_clientsecrets(), which will use a Flow
-     object to launch the browser, prompt user for authorization, and
-     store credentials.
-  """
-
-  # Credentials will be stored in ~/.credentials/chore_creds.json 
-  home_dir = os.path.expanduser('~')
-  credential_dir = os.path.join(home_dir, '.credentials')
-  if not os.path.exists(credential_dir):
-    os.makedirs(credential_dir)
-  credential_path = os.path.join(credential_dir, 'google_api_credentials.json')
-
-  store = Storage(credential_path)
-  credentials = store.get()
-  # If no credentials are already stored, run flow to get credentials
-  if not credentials or credentials.invalid:
-    print('No stored credentials. Initiating flow...')
-    flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-    flow.user_agent = APPLICATION_NAME
-    credentials = tools.run_flow(flow, store, flags)
-    print('Storing credentials to ' + credential_path)
-  return credentials
+def read_scopes(path):
+	""" Reads scopes from a text file with one scope per line 
+	"""
+	with open('/home/pi/google_credentials/scopes.txt', 'r') as scopes_file:
+			return " ".join(scopes_file.read().splitlines())
 
 
-def get_sheets_service():
-  """ Return service object for the Sheets API """
-  http = get_authorized_http()
-  sheets_discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?' 
-  			'version=v4')
-  return discovery.build('sheets', 'v4', http=http, 
-  			    discoveryServiceUrl=sheets_discoveryUrl)
+class Google_API:
+	""" Provides easier access to Google Api functions
+	Automatically gets credentials on instantiation and
+	stores an authorized httplib2.Http object as an instance
+	variable.
+	"""
+	def __init__(self, APPLICATION_NAME="APPLICATION_NAME", 
+										 CLIENT_SECRET_FILE="client_secret.json",
+										 SCOPES=None,
+										 CREDENTIALS_FILE=None):
+		self.APPLICATION_NAME = APPLICATION_NAME
+		self.CLIENT_SECRET_FILE = CLIENT_SECRET_FILE
+		self.SCOPES = SCOPES
+		self.http = self.get_authorized_http(CREDENTIALS_FILE)
 
-def get_gmail_service():
-  """ Return service object for the Gmail API """
-  http = get_authorized_http()
-  return discovery.build('gmail', 'v1', http=http)
+	def get_credentials(self, CREDENTIAL_FILE=None):
+		""" Retreive user credentials so we can access the spreadsheet.
+		This function works as follows:
+		1) Check if there are credentials already stored (in Storage object)
+		2) If there are, return the Credentials object (using store.get())
+		3) If not, initiate flow_from_clientsecrets(), which will use a Flow
+			 object to launch the browser, prompt user for authorization, and
+			 store credentials.
+		"""
+		# Credentials will be stored in ~/.credentials/google_api_credentials.json 
+		if (CREDENTIAL_FILE == None):
+			# If no path is given, use ~/.credentials
+			home_dir = os.path.expanduser('~')
+			credential_dir = os.path.join(home_dir, '.credentials')
+			if not os.path.exists(credential_dir):
+				os.makedirs(credential_dir)
+				credential_path = os.path.join(credential_dir, 'google_api_credentials.json')
+		else:
+			credential_path = CREDENTIAL_FILE 
+		# Check if there are credentials in storage. If not, run flow
+		store = Storage(credential_path)
+		credentials = store.get()
+		if not credentials or credentials.invalid:
+			print('No stored credentials. Initiating flow...')
+			flow = client.flow_from_clientsecrets(self.CLIENT_SECRET_FILE, self.SCOPES)
+			flow.user_agent = self.APPLICATION_NAME
+			credentials = tools.run_flow(flow, store, flags)
+			print('Storing credentials to ' + credential_path)
+		return credentials
 
+	def get_authorized_http(self, path=None):
+		""" Create an authorized httplib2.Http object
+		in order to acces user data
+		"""
+		credentials = self.get_credentials(path)
+		return credentials.authorize(httplib2.Http())
 
-def get_authorized_http():
-  """ Create an authorized httplib2.Http object in
-  order to acces user data
-  """
-  credentials = get_credentials()
-  return(credentials.authorize(httplib2.Http()))
+	def get_sheets_service(self):
+		""" Return service object for the Sheets API """
+		sheets_discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?' 
+					'version=v4')
+		return discovery.build('sheets', 'v4', http=self.http,
+							discoveryServiceUrl=sheets_discoveryUrl)
 
+	def get_gmail_service():
+		""" Return service object for the Gmail API """
+		return discovery.build('gmail', 'v1', http=self.http)
 
-def create_message(sender, to, subject, message_text):
-  """ Create message to send using the Gmail API
-  This function creates a MIME format email and encodes it using
-  base64 so that it can be sent using the Gmail API. To send
-  the message returned by this function, use the send_message()
-  function
-  """
-  # Construct the email using the MIME format
-  message = MIMEText(message_text)
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-  
-  """ String probelms in Python 3
-  base64.urlsafe_b64encode() takes a byte string.
-  message.as_string() returns a Python 3 str string, so we have to encode it
-  """
+	def create_message(sender, to, subject, message_text):
+		""" Create message to send using the Gmail API
+		This function creates a MIME format email and encodes it using
+		base64 so that it can be sent using the Gmail API. To send
+		the message returned by this function, use the send_message()
+		function
+		"""
+		# Construct the email using the MIME format
+		message = MIMEText(message_text)
+		message['to'] = to
+		message['from'] = sender
+		message['subject'] = subject
+		
+		""" String probelms in Python 3
+		base64.urlsafe_b64encode() takes a byte string.
+		message.as_string() returns a Python 3 str string, so we have to encode it
+		"""
 
-  byte_string = base64.urlsafe_b64encode(message.as_string().encode('ascii'))
+		byte_string = base64.urlsafe_b64encode(message.as_string().encode('ascii'))
 
-  """ More string problems in Python 3
-  base64.urlsafe_b64encode() returns a byte string, so we have to decode it
-  to be an str type string so give to the Gmail API so that the string will
-  JSON Serializable 
-  """
+		""" More string problems in Python 3
+		base64.urlsafe_b64encode() returns a byte string, so we have to decode it
+		to be an str type string so give to the Gmail API so that the string will
+		JSON Serializable 
+		"""
 
-  raw = byte_string.decode()
-  return {'raw' : raw}
+		raw = byte_string.decode()
+		return {'raw' : raw}
 
+	def send_message(service, user_id, message):
+		""" Send message to the Gmail API
+		This function assumes that service is a Service object fir the Gmail API,
+		and that message is constructed using create_message()
+		"""
 
-def send_message(service, user_id, message):
-  """ Send message to the Gmail API
-  This function assumes that service is a Service object fir the Gmail API,
-  and that message is constructed using create_message()
-  """
-
-  try:
-    message = (service.users().messages().send(userId = user_id, body=message)
-    		.execute())
-    print('Sending email')
-  except:
-    print('Error sending email')
+		try:
+			message = (service.users().messages().send(userId = user_id, body=message)
+					.execute())
+			print('Sending email')
+		except:
+			print('Error sending email')
